684955a7906d8ee783d19f324c3cc665108b09c0
B0004 <brandonkao4@gmail.com>
2023-07-28 19:57:04 -0700

    cleaned up repo

---
+++ js/about_me_script.js
@@ -0,0 +1,9 @@
+// Get the element with class name "back"
+var backElement = document.querySelector('.back');
+
+// Add a click event listener to the element
+backElement.addEventListener('click', function() {
+  // Set the new URL for navigation
+  window.location.href = 'https://b0004.github.io/personal_portfolio/';
+});
+
