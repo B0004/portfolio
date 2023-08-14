4750018fafb16beabbfb1b58d3392150ca4278a0
B0004 <brandonkao4@gmail.com>
2023-08-14 10:59:28 -0700

    added a period

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-// Get the element with class name "back"
-var backElement = document.querySelector('.back');
 
-// Add a click event listener to the element
-backElement.addEventListener('click', function() {
-  // Set the new URL for navigation
-  window.location.href = 'https://b0004.github.io/personal_portfolio/';
-});
+  //     //if deck, display
+  //     if(chosen.classList.contains("deck")){
+  //         //if already a display, history it
+  //         var cur = document.querySelector(".display");
+  //         if(cur){
+  //             cur.classList.remove("display");
+  //             cur.classList.add("history");
+  //             cur.querySelector('.front').textContent="history";
+  //         }
+  //         chosen.classList.remove("deck");
+  //         chosen.classList.add("display");
+  //     }
+  //     //if display, history
+  //     else if(chosen.classList.contains("display")){
+  //         chosen.classList.remove("display");
+  //         chosen.classList.add("history");
+  //         chosen.querySelector('.front').textContent="history";
+  //     }
+  //     //if history, display
+  //     // else if(chosen.classList.contains("history")){
+  //     //     var recent = document.getElementById(count);
+  //     //     recent.classList.remove("history");
+  //     //     recent.classList.add("display");
+  //     // }
+  // }
